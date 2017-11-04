bea9bae76131118b0cdaed4d6e07e6f389e52751
Eugeniy Popov <hey.makemescream@gmail.com>
2017-11-04 18:27:50 +0300

    addProject

---
+++ FreshChatCartageSDK/FreshChatCartageSDK/FreshChatCartageSDK.h
@@ -0,0 +1,20 @@
+//
+//  FreshChatCartageSDK.h
+//  FreshChatCartageSDK
+//
+//  Created by Eugeniy Popov on 04.11.2017.
+//  Copyright © 2017 Eugeniy Popov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Freshchat.h"
+
+//! Project version number for FreshChatCartageSDK.
+FOUNDATION_EXPORT double FreshChatCartageSDKVersionNumber;
+
+//! Project version string for FreshChatCartageSDK.
+FOUNDATION_EXPORT const unsigned char FreshChatCartageSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FreshChatCartageSDK/PublicHeader.h>
+
+
